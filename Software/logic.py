d60cb204543da664b7bb6414600d90fed83e1bfa
sahgals <shreysahgal@gmail.com>
2017-05-18 12:11:35 -0400

    Update logic.py

--- Software/logic.py
+++ Software/logic.py
@@ -1,6 +1,6 @@
-
 refresh_rate = 1000
 turn_speed = 20
+
 def ballInView():
     return True
 
@@ -14,17 +14,20 @@ def goToGoal():
 
 def turnRight():
     print("turning right")
+
 def turnLeft():
     print("turning left")
+
 def getBallCoordinates():
     return [0, 0]
+
 def move(direction, speed, len):
     print("moving")
 
 ballPos2d = [None] * 2
 
 def distFromBall(focal, width, px):
-    dist = focal*width/px
+    dist = focal * width / px
     return dist
 
 def getDirection(ball_x):
@@ -39,12 +42,17 @@ def getDirection(ball_x):
     elif ball_x >= 512 and ball_x <= 640:
         return 2
 
-while(ballInView()):
+while (ballInView()):
     
     dist = distFromBall(focal, 7, px)
-    
+
     if getDirection(getBallCoordinates()) < 0:
-        while(getDirection(getBallCoordinates()) < 0):
+        while (getDirection(getBallCoordinates()) < 0):
             turnLeft()
         goToGoal()
-    
+    elif getDirection(getBallCoordinates()) > 0:
+        while (getDirection(getBallCoordinates()) > 0):
+            turnRight()
+        goToGoal()
+    else:
+        goToGoal()
